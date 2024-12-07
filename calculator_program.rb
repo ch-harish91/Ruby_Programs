468ca6b41a032dfaf9ecf675d54ad9774893d24f
harish.ch <chinthalapudiharish@gmail.com>
2024-12-07 17:35:59 +0530

    Added a simple Ruby calculator program without methods

--- calculator_program.rb
+++ calculator_program.rb
@@ -1,36 +1,21 @@
-# Simple Calculator Program without using methods
-
-puts "Welcome to the Calculator Program!"
-
-# Ask the user for two numbers
-print "Enter the first number: "
+puts "Enter the first number: "
 num1 = gets.to_f
 
-print "Enter the second number: "
+puts "Enter the second number: "
 num2 = gets.to_f
 
-puts "Select an operation:"
-puts "1. Add"
-puts "2. Subtract"
-puts "3. Multiply"
-puts "4. Divide"
-operation = gets.to_i
-if operation == 1
-  result = num1 + num2
-  puts "The result of adding #{num1} and #{num2} is #{result}."
-elsif operation == 2
-  result = num1 - num2
-  puts "The result of subtracting #{num1} from #{num2} is #{result}."
-elsif operation == 3
-  result = num1 * num2
-  puts "The result of multiplying #{num1} and #{num2} is #{result}."
-elsif operation == 4
-  if num2 == 0
-    puts "Error: Division by zero is not allowed."
-  else
-    result = num1 / num2
-    puts "The result of dividing #{num1} by #{num2} is #{result}."
-  end
+# Perform all four operations at once
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+if num2 == 0
+  division = "Error: Division by zero is not allowed."
 else
-  puts "Invalid operation selected!"
+  division = num1 / num2
 end
+
+# Output all results
+puts "The result of adding #{num1} and #{num2} is #{addition}."
+puts "The result of subtracting #{num2} from #{num1} is #{subtraction}."
+puts "The result of multiplying #{num1} and #{num2} is #{multiplication}."
+puts "The result of dividing #{num1} by #{num2} is #{division}."
