ddd3463af333e08a799c0e9a1a9b0769c4637188
harish.ch <chinthalapudiharish@gmail.com>
2024-12-08 07:59:13 +0530

    Added a program to calculate the sum of natural numbers

---
+++ sum_of_natural_numbers.rb
@@ -0,0 +1,9 @@
+
+n = 10  # You can change this value as needed
+# Initialize sum to 0
+sum = 0
+# Calculate the sum of natural numbers
+for i in 1..n
+  sum += i
+end
+puts "The sum of natural numbers up to #{n} is #{sum}."
