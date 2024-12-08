0affc224178d1b9beae23e322c1aee904c3bb735
harish.ch <chinthalapudiharish@gmail.com>
2024-12-08 08:04:34 +0530

    Added program to print English alphabets

---
+++ print_alphabets.rb
@@ -0,0 +1,15 @@
+# Print uppercase alphabets
+puts "Uppercase Alphabets:"
+for char in 'A'..'Z'
+  print "#{char} "
+end
+
+puts "\n\nLowercase Alphabets:"
+
+# Print lowercase alphabets
+for char in 'a'..'z'
+  print "#{char} "
+end
+
+puts
+
