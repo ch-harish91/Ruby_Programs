f84a47b0423b5ef324824a51f395c9a82dc657f6
harish.ch <chinthalapudiharish@gmail.com>
2024-12-08 08:22:43 +0530

    Added program to check if a number is positive or negative

---
+++ check_number.rb
@@ -0,0 +1,11 @@
+# Directly assign the number here
+number = 10  # Example number, you can change it
+
+# Check if the number is positive, negative, or zero
+if number > 0
+  puts "#{number} is a positive number."
+elsif number < 0
+  puts "#{number} is a negative number."
+else
+  puts "The number is zero."
+end
