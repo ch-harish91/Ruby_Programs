f38ac526625c53f07dcda1a9da839eef21d36411
harish.ch <chinthalapudiharish@gmail.com>
2024-12-13 04:41:19 +0530

    Add Diamond Pattern program

---
+++ diamond_pattern.rb
@@ -0,0 +1,26 @@
+n = 5  # Number of rows for the upper part
+
+# Upper part (Pyramid shape)
+for i in 1..n
+  # Print spaces
+  for j in 1..(n - i)
+    print " "
+  end
+  # Print stars
+  for k in 1..(2 * i - 1)
+    print "*"
+  end
+  puts
+end
+# Lower part (Inverted pyramid shape)
+for i in (n-1).downto(1)
+  # Print spaces
+  for j in 1..(n - i)
+    print " "
+  end
+  # Print stars
+  for k in 1..(2 * i - 1)
+    print "*"
+  end
+  puts
+end
