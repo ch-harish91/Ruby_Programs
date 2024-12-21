822ec8138666c95d6561ff91e39bbcaa1e4a1839
harish.ch <chinthalapudiharish@gmail.com>
2024-12-21 07:47:12 +0530

    Removed pyramid_pattern.rb.save

--- count_words.rb
+++
@@ -1,9 +0,0 @@
-# Get the sentence from the user
-puts "Enter a sentence:"
-sentence = gets.chomp
-
-# Split the sentence into words and count them
-word_count = sentence.split.count
-
-# Output the result
-puts "Word count: #{word_count}"
